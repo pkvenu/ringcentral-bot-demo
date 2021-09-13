4a5b09e5818cd3cd54e90079b0e7b36428938187
byrne.reese <byrne@majordojo.com>
2021-09-13 16:19:10 -0700

    Updates to app.js during regular QA

--- app.js
+++ app.js
@@ -63,8 +63,9 @@ app.post('/oauth', function (req, res) {
 	data.token_type = "bearer"
 	data.expires_in = 1000000
 	data.access_token = bot_token
-	data.refresh_token = 'xxx'
-	data.refresh_token_expires_in = 1000000
+	//data.refresh_token = 'xxx'
+	//data.refresh_token_expires_in = 1000000
+	console.log("Setting auth context to: ", data)
 	platform.auth().setData(data)    
 	
 	// Subscribe to webhooks relating to team messaging posts. This
@@ -84,26 +85,20 @@ app.post('/oauth', function (req, res) {
 // types a message to your bot. 
 app.post('/callback', function (req, res) {
     var validationToken = req.get('Validation-Token');
-    var body =[];
-    console.log("Webhook received.")
-    if(validationToken) {
-        console.log('Responding to RingCentral as last leg to create new Webhook');
+    var body = [];
+    if (validationToken) {
+        console.log('Verifying webhook.');
         res.setHeader('Validation-Token', validationToken);
         res.statusCode = 200;
         res.end();
     } else {
-        req.on('data', function(chunk) {
-            body.push(chunk);
-        }).on('end', function() {
-            body = Buffer.concat(body).toString();
-            console.log('WEBHOOK EVENT BODY: ', body);
-            var obj = JSON.parse(body);
-            res.statusCode = 200;
-            res.end(body);
-            if(obj.event == "/restapi/v1.0/subscription/~?threshold=60&interval=15"){
-                renewSubscription(obj.subscriptionId);
-            }
-        });
+	console.log("Webhook received: ", req.body);
+	res.statusCode = 200;
+        res.end('');
+        if (req.body.event == "/restapi/v1.0/subscription/~?threshold=60&interval=15") {
+	    console.log("Renewing subscription ID: " + req.body.subscriptionId);
+            renewSubscription(req.body.subscriptionId);
+        }
     }
 });
 
